9bfdb7e9d20edf48445874b2fc6349a252b4e4cf
BrovkinOleg <boxline@yandex.ru>
2019-07-31 10:32:21 +0300

    file TriangleArea.rb is ready

---
+++ Basic_Ruby_task_01/TriangleArea.rb
@@ -0,0 +1,10 @@
+
+# author Brovkin Oleg
+# 31.07.2019
+puts "Enter length of the base (cm), please"
+length_of_base = gets.chomp
+puts "Enter height of triangle (cm)"
+height = gets.chomp
+area = height.to_i * length_of_base.to_i / 2
+
+puts "Area of the triangle is #{area}"
