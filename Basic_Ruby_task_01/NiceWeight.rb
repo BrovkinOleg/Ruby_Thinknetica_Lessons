3648fd20ddf2002cd6b4e9d4d675bb301d06bcf3
BrovkinOleg <boxline@yandex.ru>
2019-07-31 10:12:38 +0300

    Name must be from capital letter

--- Basic_Ruby_task_01/NiceWeight.rb
+++ Basic_Ruby_task_01/NiceWeight.rb
@@ -2,7 +2,7 @@
 # author Brovkin Oleg
 # 31.07.2019
 puts "Enter your name, please"
-name = gets.chomp
+name = gets.chomp.capitalize!
 puts "Enter your height, please"
 height = gets.chomp
 weight = height.to_i - 110
