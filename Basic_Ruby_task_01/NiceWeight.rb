8ff10f574e8d150dfd9b2f4af66c0c59464fae4a
BrovkinOleg <boxline@yandex.ru>
2019-07-31 09:53:29 +0300

    NiceWeight.rb is ready

---
+++ Basic_Ruby_task_01/NiceWeight.rb
@@ -0,0 +1,13 @@
+
+# author Brovkin Oleg
+# 31.07.2019
+puts "Enter your name, please"
+name = gets.chomp
+puts "Enter your height, please"
+height = gets.chomp
+weight = height.to_i - 110
+if weight < 0
+  puts "You have the optimal weight"
+else
+  puts "#{name}, your weight must be #{weight}"
+end
