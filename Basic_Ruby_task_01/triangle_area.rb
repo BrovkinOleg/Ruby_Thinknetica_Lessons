ae178aec6be1aaba59099df3bc865c167778ef86
BrovkinOleg <boxline@yandex.ru>
2019-08-01 15:00:04 +0300

    elimination of errors and remarks from thinknetica

--- Basic_Ruby_task_01/triangle_area.rb
+++ Basic_Ruby_task_01/triangle_area.rb
@@ -3,9 +3,9 @@
 # author Brovkin Oleg
 # 31.07.2019
 puts 'Enter length of the base (cm), please'
-length_of_base = gets.chomp
+length_of_base = gets.chomp.to_i
 puts 'Enter height of triangle (cm)'
-height = gets.chomp
-area = height.to_i * length_of_base.to_i / 2
+height = gets.chomp.to_i
+area = height * length_of_base / 2
 
 puts "Area of the triangle is #{area}"
