82f38184091fb0cd49ba1fdbfa1f7d254d390c11
BrovkinOleg <boxline@yandex.ru>
2019-08-01 08:01:34 +0300

    four files ready for check

---
+++ Basic_Ruby_task_01/QuadraticEquation.rb
@@ -0,0 +1,31 @@
+# author Brovkin Oleg
+# 31.07.2019
+puts "Enter parameter \"a\", please"
+a = gets.chomp.to_i
+puts "Enter parameter \"b\", please"
+b = gets.chomp.to_i
+puts "Enter parameter \"c\", please"
+c = gets.chomp.to_i
+
+D = (b*b - 4*a*c) / 2
+
+if D >= 0
+  root_of_d = Math.sqrt(D)
+else
+  root_of_d = 0
+end
+
+X1 = (-b + root_of_d) / 2
+X2 = (-b - root_of_d) / 2
+
+if D > 0
+  puts "X1 = #{X1}, X2 = #{X2}"
+elsif D == 0
+  puts "D == 0, so X1 = X2 = #{X1}"
+else
+  puts "D < 0, no solutions"
+end
+
+
+
+
